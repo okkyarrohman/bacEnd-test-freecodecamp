b344b667fecc882ec09cee5802bf83631f37a5f9
okkyarrohman <okkyzee@gmail.com>
2023-03-19 13:17:57 +0700

    okay

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ if (!process.env.DISABLE_XORIGIN) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     var origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        console.log(origin);
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
     next();
   });
